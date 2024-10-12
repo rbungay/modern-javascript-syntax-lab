eb9819238e521bd31a8e297beb3f458492430639
Rani Bungay <ranibungay@gmail.com>
2024-10-12 11:28:03 -0400

    init commit

---
+++ app.jsx
@@ -0,0 +1,21 @@
+// Use `.map()` to iterate over the following array:
+
+const nums = [13, 87, 2, 89, 12, 4, 90, 63];
+
+// Create a new array where each value is multiplied by 2 and log the new array.
+
+// Your code here
+
+const times2 = nums.map((num) => num * 2);
+
+console.log(`Exercise 1 complete: ${times2}`);
+
+// Given the following array, use destructuring to pull out the first and second values and place them into variables. Log both variables.
+
+const pizzaToppings = ["Pineapple", "Olives", "Anchovies"];
+
+// Your code here
+
+const [firstTopping, secondTopping] = pizzaToppings;
+
+console.log(`Exercise 2 complete: ${firstTopping} and ${secondTopping}`);
